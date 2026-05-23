806256406e773e363e828510492554027e8430e6
Espadas23 <niknik23@inbox.lv>
2026-05-23 13:23:04 +0300

    our hope

--- Assets/Scripts/NoteMissZone.cs
+++ Assets/Scripts/NoteMissZone.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+/*using UnityEngine;
 
 public class NoteMissZone : MonoBehaviour
 {
@@ -29,5 +29,54 @@ public class NoteMissZone : MonoBehaviour
             return;
 
         note.DestroyAndRespawn();
+    }
+}*/
+
+
+using UnityEngine;
+
+public class NoteMissZone : MonoBehaviour
+{
+    [Header("Настройки")]
+    public bool teleportOnlyOnTriggerExit = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (teleportOnlyOnTriggerExit)
+            return;
+
+        TeleportNote(other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!teleportOnlyOnTriggerExit)
+            return;
+
+        TeleportNote(other);
+    }
+
+    private void TeleportNote(Collider other)
+    {
+        NoteObject note = other.GetComponentInParent<NoteObject>();
+
+        if (note == null || note.IsUsed())
+            return;
+
+        if (note.respawnPoint == null)
+            return;
+
+        Rigidbody rb = note.GetComponent<Rigidbody>();
+
+        // Сбрасываем физику
+        if (rb != null)
+        {
+            rb.linearVelocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+
+        // Телепортируем обратно
+        note.transform.position = note.respawnPoint.position;
+        note.transform.rotation = note.respawnPoint.rotation;
     }
 }
